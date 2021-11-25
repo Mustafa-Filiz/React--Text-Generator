2c9fd261cd7c9a511d429bb268c9b1f934c839cd
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-25 14:41:46 +0300

    updates

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { formatSelector, textSelector } from '../redux/textSlice';
 
 function Display() {
-	return (
-		<div>
-			Bacom ipsum
-		</div>
-	)
+    const text = useSelector(textSelector);
+    const format = useSelector(formatSelector);
+
+    return (
+        <div>
+            {text && text.map((p) =>
+                format === 'html' ? <p>{`<p>${p}</p>`}</p> : <p>{p}</p>
+            )}
+        </div>
+    );
 }
 
-export default Display
+export default Display;
